11880eb725c1cd285fdcae79b42c1479be13eaf1
Amazingjosh <theamazingkeyz@gmail.com>
2025-06-18 13:31:34 +0100

    debugging

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/" sizes="any" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
